84868e3c1b39dbcf4199727a33d86b4f090094d5
Qualidade & Produtividade <fernando_ferrari_f@hotmail.com>
2025-05-07 23:55:06 -0300

    Create Intro.R

---
+++ Intro.R
@@ -0,0 +1,28 @@
+# Introdução às operações básicas no R
+
+# Atribuição de variáveis
+a <- 10       # Usando o operador de atribuição "<-"
+b <- 5
+
+# Operações aritméticas básicas
+soma <- a + b          # Soma
+subtracao <- a - b     # Subtração
+multiplicacao <- a * b # Multiplicação
+divisao <- a / b       # Divisão
+potencia <- a^b        # Potência
+
+# Impressão dos resultados
+print(paste("Soma: ", soma))
+print(paste("Subtração: ", subtracao))
+print(paste("Multiplicação: ", multiplicacao))
+print(paste("Divisão: ", divisao))
+print(paste("Potência: ", potencia))
+
+# Usando funções embutidas
+media <- mean(c(a, b))
+maximo <- max(a, b)
+minimo <- min(a, b)
+
+print(paste("Média: ", media))
+print(paste("Máximo: ", maximo))
+print(paste("Mínimo: ", minimo))
