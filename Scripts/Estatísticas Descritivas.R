b34a8ec607dc583ce39de15ef40a9721b0856ed9
FerrariFFF-1980 <fernando_ferrari_f@hotmail.com>
2026-03-02 16:39:03 -0300

    Adicao do script de estatisticas descritivas

---
+++ Scripts/Estatísticas Descritivas.R
@@ -0,0 +1,29 @@
+# Média Aritmética
+# Diâmetro médio de eixo / Espessura média de chapa / Peso médio de envase
+mean(PesoPeixes$Peso)
+
+# Mediana
+# Medida de Tendência Central robusta a valores extremos
+median(PesoPeixes$Peso)
+median(PesoPeixes$PesoExtremo)
+
+# Moda
+# Valor mais frequente (que mais se repete)
+tabela <- table(PesoPeixes$Peso)
+moda <- as.numeric(names(tabela)[tabela == max(tabela)])
+
+# Desvio Padrão
+# Medida de dispersão que indica o quanto os dados estão afastados da média
+sd(PesoPeixes$Peso)
+sd(PesoPeixes$PesoExtremo)
+
+# Coeficiente de Variação
+# Medida de dispersão relativa, em porcentagem, indica a variabilidade dos dados em relação à média
+cv <- (sd(PesoPeixes$Peso) / mean(PesoPeixes$Peso)) * 100
+
+# Estatística Descritiva
+# Consolidado com várias estatísticas descritivas
+# Usando o pacote DescTools
+# install.packages("DescTools")
+library(DescTools)
+Desc(PesoPeixes$Peso)
