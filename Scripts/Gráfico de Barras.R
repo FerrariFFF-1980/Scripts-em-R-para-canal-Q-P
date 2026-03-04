c3b7ad2aff32faba07e43dce88fe1fa21424aa76
FerrariFFF-1980 <fernando_ferrari_f@hotmail.com>
2026-03-04 15:03:09 -0300

    Adicionando script para graficos de colunas e barras

---
+++ Scripts/Gráfico de Barras.R
@@ -0,0 +1,63 @@
+# Gráfico de Colunas e Barras
+
+# Pacote ggplot2
+library(ggplot2)
+
+# Barras empilhadas
+ggplot(falhas_pcb, aes(x = Shift, fill = Line)) +
+  
+  geom_bar(color = "white") +
+  
+  geom_text(
+    stat = "count",
+    aes(label = after_stat(count)),
+    position = position_stack(vjust = 0.5),
+    size = 5,
+    color = "black"
+  ) +
+  
+  labs(
+    title = "Falhas por Linha e Turno",
+    x = "Turno",
+    y = "Frequência",
+    fill = "Linha"
+  ) +
+  
+  scale_fill_brewer(palette = "Set2") +
+  theme_light(base_size = 16) +
+  theme(legend.position = "right")
+  coord_flip()  # se quiser em barras horizontais
+
+
+# Barras lado a lado
+ggplot(falhas_pcb, aes(x = Shift, fill = Line)) +
+  
+  geom_bar(
+    width = 0.8,
+    position = "dodge",
+    color = "white"
+  ) +
+  
+  geom_text(
+    stat = "count",
+    aes(label = after_stat(count)),
+    position = position_dodge(width = 0.8),
+    vjust = -0.3,
+    size = 5,
+    color = "black"
+  ) +
+  
+  scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+  
+  labs(
+    title = "Falhas por Linha e Turno",
+    x = "Turno",
+    y = "Frequência",
+    fill = "Linha"
+  ) +
+  
+  scale_fill_brewer(palette = "Set2") +
+  theme_light(base_size = 16) +
+  theme(legend.position = "right")
+# coord_flip()  # se quiser em barras horizontais (vjust muda para hjust)
+ 
