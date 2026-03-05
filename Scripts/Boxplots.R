8c9026ccbcf43cbbdee87eb64beae4d8f74e9c89
FerrariFFF-1980 <fernando_ferrari_f@hotmail.com>
2026-03-05 20:17:40 -0300

    Correções no script e dataset para boxplot - pós-video

--- Scripts/Boxplots.R
+++ Scripts/Boxplots.R
@@ -19,32 +19,32 @@ CentrosUsinagem %>%
   
     # Escala Okabe-Ito
     scale_fill_manual(
-    values = c(
-      "#0072B2",  # azul
-      "#E69F00",  # laranja
-      "#009E73"   # verde
-      # "#D55E00",  # vermelho
-      # "#CC79A7",  # roxo
-      # "#56B4E9",  # azul claro
-      # "#F0E442",  # amarelo
-      # "#000000"   # preto
-    )
+      values = c(
+        "#0072B2",  # azul
+        "#E69F00",  # laranja
+        "#009E73"   # verde
+        # "#D55E00",  # vermelho
+        # "#CC79A7",  # roxo
+        # "#56B4E9",  # azul claro
+        # "#F0E442",  # amarelo
+        # "#000000"   # preto
+      )
   ) +
   
   # Opcional - linhas dos limites de especificação
-  # geom_hline(
-  #   yintercept = c(25.05, 24.95), # Intervalo de especificação
-  #   linetype = "dashed",
-  #   linewidth = 0.5,
-  #   color = "#D55E00"
-  # ) +
+  geom_hline(
+    yintercept = c(25.05, 24.95), # Intervalo de especificação
+    linetype = "dashed",
+    linewidth = 0.5,
+    color = "#D55E00"
+  ) +
   
-  # Opcional - adição de jitters
-  # geom_jitter(
-  #   color="black",
-  #   size=0.4,
-  #   alpha=0.9
-  # ) +
+
+  geom_jitter(
+    color="black",
+    size=0.4,
+    alpha=0.9
+  ) +
   
   theme_light() +
   
@@ -56,7 +56,7 @@ CentrosUsinagem %>%
   labs (
     title = "Distribuição dos Diâmetros do Eixos [mm]",
     subtitle = "por Centros de Usinagem"
-  ) 
+  ) +
 
 # Opcional - girar 90°
-# coord_flip()
+coord_flip()
