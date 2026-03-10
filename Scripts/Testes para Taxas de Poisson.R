6b0e396913399a4c5639de4baf1051b63a35bbb5
FerrariFFF-1980 <fernando_ferrari_f@hotmail.com>
2026-03-10 13:31:11 -0300

    Revisao do script de teste de Poisson

--- Scripts/Testes para Taxas de Poisson.R
+++ Scripts/Testes para Taxas de Poisson.R
@@ -2,20 +2,12 @@
 # Teste de Poisson para 1 amostra #
 ###################################
 
-# Número total de eventos observados
-numero_defeitos <- 18
-
-# Número de unidades inspecionadas
-numero_pecas <- 200
-
-# Taxa hipotética de defeitos
-taxa_hipotetica <- 0.05
-
 # Teste de Poisson
 poisson.test(
-  x = numero_defeitos,
-  T = numero_pecas,
-  r = taxa_hipotetica,
+  x = 18,
+  T = 200,
+  r = 0.05,
+  conf.level = 0.95,
   alternative = "two.sided"
 )
 
@@ -23,15 +15,10 @@ poisson.test(
 # Teste de Poisson para 2 amostras #
 ####################################
 
-# Número de eventos
-eventos <- c(14, 7)
-
-# Exposição (peças inspecionadas)
-exposicao <- c(180, 160)
-
 # Teste de igualdade das taxas
 poisson.test(
-  x = eventos,
-  T = exposicao,
+  x = c(14, 7),
+  T = c(180, 160),
+  conf.level = 0.95,
   alternative = "two.sided"
 )
