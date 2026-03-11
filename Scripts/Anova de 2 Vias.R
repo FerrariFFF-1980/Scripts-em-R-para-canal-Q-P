ba05c6340e9b40d1be85d2c0f87b2ff4905e6cb6
FerrariFFF-1980 <fernando_ferrari_f@hotmail.com>
2026-03-11 07:48:14 -0300

    Revisao do script de Anova de 2 vias

--- Scripts/Anova de 2 Vias.R
+++ Scripts/Anova de 2 Vias.R
@@ -1,5 +1,5 @@
 #################################
-# TWO-WAY ANOVA                 #  
+# TWO-WAY ANOVA                 #
 # Anova de 2 vias - ANOVA2.xlsx #
 #################################
 
@@ -27,71 +27,37 @@ okabe_ito <- c(
   "#0072B2",
   "#D55E00",
   "#CC79A7",
-  "#000000"
-)
+  "#000000")
 
 # Boxplots
 ggplot(
   ANOVA2,
-  aes(
-    x = CentroUsinagem,
-    y = Diametro_mm,
-    fill = VelocidadeCorte
-  )
-) +
-  geom_boxplot(
-    alpha = 0.8,
-    position = position_dodge(width = 1)
-  ) +
-  scale_fill_manual(
-    values = c(
-      okabe_ito[2],
-      okabe_ito[6])
-    ) +
-  
+  aes(x = CentroUsinagem, y = Diametro_mm, fill = VelocidadeCorte)) +
+  geom_boxplot(alpha = 0.8, position = position_dodge(width = 1)) +
+  scale_fill_manual(values = c(okabe_ito[2], okabe_ito[6])) +
   labs(
     title = "Diâmetro por Centro de Usinagem e Velocidade de Corte",
-    x = "Centro de Usinagem",
-    y = "Diâmetro (mm)"
-  ) +
+    x = "Centro de Usinagem", y = "Diâmetro (mm)") +
   theme_light()
 
 # Gráfico de Interações
 ggplot(
   ANOVA2,
-  aes(
-    x = VelocidadeCorte,
-    y = Diametro_mm,
-    color = CentroUsinagem,
-    group = CentroUsinagem
-  )
-) +
-  stat_summary(
-    fun = mean,
-    geom = "point",
-    size = 3
-  ) +
-  stat_summary(
-    fun = mean,
-    geom = "line",
-    linewidth = 1
-  ) +
-  scale_color_manual(
-    values = okabe_ito[1:3]
-  ) +
+  aes(x = VelocidadeCorte, y = Diametro_mm,
+    color = CentroUsinagem, group = CentroUsinagem)) +
+  stat_summary(fun = mean, geom = "point", size = 3) +
+  stat_summary(fun = mean, geom = "line", linewidth = 1) +
+  scale_color_manual(values = okabe_ito[1:3]) +
   
   labs(
     title = "Gráfico de Interação",
     x = "Velocidade de Corte",
-    y = "Média do Diâmetro (mm)"
-  ) +
+    y = "Média do Diâmetro (mm)") +
   theme_light()
 
 # Modelo Two-Way Anova
-modelo_two_way <- aov(
-  Diametro_mm ~ CentroUsinagem * VelocidadeCorte,
-  data = ANOVA2
-)
+modelo_two_way <- aov(Diametro_mm ~ CentroUsinagem * VelocidadeCorte,
+  data = ANOVA2)
 
 summary(modelo_two_way)
 
@@ -107,16 +73,9 @@ dados_residuos <- data.frame(
 # Resíduos vs. Valores Ajustados
 ggplot(
   dados_residuos,
-  aes(
-    x = Ajustados,
-    y = Residuos
-  )
-) +
+  aes(x = Ajustados, y = Residuos)) +
   geom_point(size = 3, color = okabe_ito[5]) +
-  geom_hline(
-    yintercept = 0,
-    linetype = "dashed"
-  ) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
   labs(
     title = "Resíduos vs Ajustados",
     x = "Valores Ajustados",
@@ -127,16 +86,10 @@ ggplot(
 # Gráfico de Normalidade de Resíduos (Q-Q)
 ggplot(
   data.frame(residuos),
-  aes(sample = residuos)
-) +
-  stat_qq(
-    color = okabe_ito[6],
-    size = 3
-  ) +
+  aes(sample = residuos)) +
+  stat_qq(color = okabe_ito[6], size = 3) +
   stat_qq_line(color = "black") +
-  labs(
-    title = "Gráfico Q-Q dos Resíduos"
-  ) +
+  labs(title = "Gráfico Q-Q dos Resíduos") +
   theme_light()
 
 # Teste de Normalidade dos Resíduos
