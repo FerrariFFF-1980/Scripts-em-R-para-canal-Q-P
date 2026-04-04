55774d6b47c188744e67212b6a563fd4a1055801
FerrariFFF-1980 <fernando_ferrari_f@hotmail.com>
2026-04-04 09:33:58 -0300

    Update no script do Teste de Kendau Tau

--- Scripts/Teste de Kendal Tau.R
+++ Scripts/Teste de Kendal Tau.R
@@ -1,8 +1,8 @@
 # Teste de Associação Não-paramétrico de Kendall Tau ----
-# Associação monotônica (sempre crescente ou decrescente)
+# Associação monotônica (crescente ou decrescente)
 # Dados ruidosos ou com outliers
 # Escala ordinal ou ranking
-# Amostra pequena
+# Amostra pequena ----
 
 # Configuracoes ----
 options(scipen = 999)
@@ -10,54 +10,45 @@ options(scipen = 999)
 # Pacotes ----
 library(ggplot2)
 library(dplyr)
-library(tidyr)
 
 # Dados ----
 dados <- data.frame(
   temperatura = c(20, 22, 25, 27, 30, 32, 35, 37, 40, 42),
-  desgaste    = c(5,   6,  7,  9, 10, 12, 11, 15, 16, 18))
+  desgaste    = c(5, 6, 7, 9, 10, 12, 11, 15, 16, 18))
 
-# Gerar todos os pares ----
+# Gerar todos os pares (i, j) ----
 pares <- expand.grid(i = 1:nrow(dados), j = 1:nrow(dados)) %>%
-  filter(i < j) %>%  # garante pares únicos
+  filter(i < j) %>%
   mutate(
     x_i = dados$temperatura[i],
     x_j = dados$temperatura[j],
     y_i = dados$desgaste[i],
     y_j = dados$desgaste[j],
     
-    # diferenças
     diff_x = x_i - x_j,
     diff_y = y_i - y_j,
     
-    # produto das diferenças
     produto = diff_x * diff_y,
     
-    # classificação
     classificacao = case_when(
       produto > 0 ~ "Concordante",
       produto < 0 ~ "Discordante",
-      TRUE ~ "Empate"))
-
+      TRUE ~ "Empate" ))
 
 # Resumo dos pares ----
-resumo <- pares %>% count(classificacao)
+pares %>% count(classificacao)
 
-print(resumo)
-
-# Cálculo manual ----
+# Calculo manual do Kendall Tau ----
 C <- sum(pares$classificacao == "Concordante")
 D <- sum(pares$classificacao == "Discordante")
 n <- nrow(dados)
-
 total_pares <- n * (n - 1) / 2
-
 tau_manual <- (C - D) / total_pares
 
-# Comparação com Teste de Correlação ----
+# Comparacao com cor.test ----
 cor.test(dados$temperatura, dados$desgaste, method = "kendall")
 
-# Gráfico ----
+# Grafico ----
 ggplot(dados, aes(x = temperatura, y = desgaste)) +
   geom_point(size = 3, color = "#0072B2") +
   geom_smooth(method = "lm", se = FALSE, color = "#D55E00") +
